0b8b28c239f10985a393352b6d7de0bb0aab4890
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-11-25 03:36:19 -0500

    Add tests

--- Tester.java
+++ Tester.java
@@ -10,9 +10,10 @@ public class Tester {
         System.out.println(a.equals(new RealNumber(0))); // False
 
         RationalNumber zero = new RationalNumber(5, 0);
-        RealNumber threeHalves = zero.add(new RationalNumber(10, 15).reciprocal());
-        System.out.println(((RationalNumber) threeHalves).divide(new RationalNumber(2, 4)).multiply(new RationalNumber(9, 3)).subtract(new RationalNumber(9, 1)).equals(zero));
-        System.out.println(zero);
-        System.out.println(new RationalNumber((int)1e9, (int)1e6));
+        Number threeHalves = zero.add(new RationalNumber(10, 15).reciprocal());
+        System.out.println(((RationalNumber) threeHalves).divide(new RationalNumber(2, 4)).multiply(new RationalNumber(9, 3)).subtract(new RationalNumber(9, 1)).equals(zero)); // True
+        System.out.println(((RationalNumber) threeHalves).divide(new RationalNumber(2, 4)).multiply(new RationalNumber(9, 3)).subtract(new RationalNumber(9, 1)).equals(new RealNumber(0))); // True
+        System.out.println(zero); // 0
+        System.out.println(new RationalNumber((int)1e9, (int)1e6)); // 1000
     }
 }
